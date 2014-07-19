6786840ab7ed864bc451855d33c269fe5da7199c
Mark Anthony Andes <andesonline20@gmail.com>
2014-07-19 21:46:43 +0800

    Change UI of Home (site/index.php)

--- trunk/application/mopccss/protected/views/site/index.php
+++ trunk/application/mopccss/protected/views/site/index.php
@@ -3,18 +3,53 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<img src="logo.png" alt="Smiley face" style="float: left">
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<table style="float: right"> 
+    <tr> <td><h2 style="float: right">Calendar: </h2> </td> </tr>
+    <tr><td><input type="date" style="float: right"> </td></tr>
+</table>
+
+<head>
+
+<style>
+body {font-family:Arial, Helvetica, sans-serif; font-size:12px;}
+ 
+.fadein { 
+position:relative; height:332px; width:500px; margin:0 auto;
+background: url("slideshow-bg.png") repeat-x scroll left top transparent;
+padding: 10px;
+ }
+.fadein img { position:absolute; left:50px; top:80px; }
+
+
+
+
+</style>
+ 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>
+$(function(){
+	$('.fadein img:gt(0)').hide();
+	setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+});
+</script>
+ 
+</head>
+<body>
+<div class="fadein">
+<img src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xpa1/t1.0-9/10347545_4456510787838_5598134013676143985_n.jpg">
+<img src="https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpa1/t1.0-9/10537363_4456510227824_3020636130644429675_n.jpg">
+<img src="https://scontent-a-lax.xx.fbcdn.net/hphotos-xpf1/t1.0-9/10550938_4456511747862_5425212068525909161_n.jpg">
+<img src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xap1/t1.0-9/10411989_4456512627884_6604271508412173389_n.jpg">
+</div>
+    
+    
+     
+</body>
+
+
+
