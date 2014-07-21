d67fca177f78299093972312dc45e7317cd8ca58
John Emmnauel Tero <jejtero@gmail.com>
2014-07-21 15:12:21 +0800

    edited about.php in yii

--- trunk/application/mopccss/protected/views/site/pages/about.php
+++ trunk/application/mopccss/protected/views/site/pages/about.php
@@ -8,29 +8,28 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<b><font color=#808080>Military Ordinariate of the Philippines Profile</font></b>
+<img src="http://directory.ucanews.com/uploads/images/1357896339.jpg"" alt="mop logo" width="100" height="100">
+<b><font color=#808080><h3>Military Ordinariate of the Philippines Profile</h3></font></b>
 
-<p>About MOP
+<p>The Military Ordinariate of the Philippines or MOP is a personal diocese of men and women in uniform or for the different branch of service. The said branch of service includes the Armed Forces of the Philippines (A.F.P.), Philippine National Police (P.N.P.), Bureau of Jail Management and Penology (B.J.M.P.), and the Philippine Coast Guard. </p>
 
-The Military Ordinariate of the Philippines or MOP is a personal diocese of men and women in uniform or for the different branch of service. The said branch of service includes the Armed Forces of the Philippines (A.F.P.), Philippine National Police (P.N.P.), Bureau of Jail Management and Penology (B.J.M.P.), and the Philippine Coast Guard. 
-
-<p>
-Armed Forces of the Philippines (AFP) </br>
+<p><img src="http://www.alconphils.com/wp-content/uploads/2014/02/afp.jpg" alt="afp logo" width="100" height="100">
+<h3>Armed Forces of the Philippines (AFP)</h3> </br>
 This branch of the government is dependent to the Military Ordinariate of the Philippines as its own diocese. Also, it is one of the main branches of service that the diocese is handling. It is a volunteer force that is composed of three major branches: Philippine Army, Philippine Navy, and the Philippine Army. All under the Armed Forces of the Philippines is privileged to store their personal data including their Baptism, Confirmation and Wedding Certificate thus making the data secured and stored in one unique file system which is in MOP.
 </p>
 
-<p>
-Philippine National Police (PNP) </br>
+<p><img src="http://www.emb.gov.ph/portal/portals/40/Images/logo/pnp-logo.png" alt="pnp logo" width="100" height="100">
+<h3>Philippine National Police (PNP)</h3> </br>
 This is the civilian national police force of the Philippines that aims to protect civilians and enforce the law. PNP is also part of the main dependent organization of the Military Ordinariate of the Philippines. All of the personal files of these men including their Baptism, Confirmation and Wedding Certificates are all going to be taken care of the Military Ordinariate of the Philippines.
 </p>
 
-<p>	
-Philippine Coast Guard </br>
+<p><img src="http://globalnation.inquirer.net/files/2013/01/pcg-logo.jpg" alt="pcg logo" width="100" height="100">	
+<h3>Philippine Coast Guard</h3> </br>
 This government organization provides armed and uniformed service and also enforces law within Philippine waters. As one of the main branch of the Military Ordinariate of the Philippines it is also one of the main dependent branch of government that needs the service of MOP when it comes to the storage of Baptism, Confirmation and Wedding Certificates which are all important files.
 </p>
 
-<p>
-Bureau of Jail Management and Penology </br>
+<p><img src="http://lakankali.com/images/bjmp_logo.png" alt="bjmp logo" width="100" height="100">
+<h3>Bureau of Jail Management and Penology</h3> </br>
 Is a branch of government that concerns the jail management and penology problems. This organization will need the services of the Military Ordinariate of the Philippines for the handling of Baptism, Confirmation and Wedding Certificates which is all-known as important and confidential files.
 </p>
 
