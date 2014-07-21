95d96c2dc8a9ebded7b081831a3e3a03bfae3784
Gene Anthony Kadano <gskadano@gmail.com>
2014-07-21 16:38:36 -0700

    modified Kadano_GeneAnthony_midterms/sysadd1_kadano_geneanthony_ourspace.sql and Kadano_GeneAnthony_midterms/ouspace/css/main.css

--- trunk/midterm_test/Kadano_GeneAnthony_midterms/sysadd1_kadano_geneanthony_ourspace.sql
+++ trunk/midterm_test/Kadano_GeneAnthony_midterms/sysadd1_kadano_geneanthony_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2014 at 01:19 AM
+-- Generation Time: Jul 22, 2014 at 01:37 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -33,6 +33,9 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `lastname` varchar(30) NOT NULL,
   `gender` varchar(1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `home_address` varchar(50) DEFAULT NULL,
+  `landline` varchar(20) DEFAULT NULL,
+  `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
