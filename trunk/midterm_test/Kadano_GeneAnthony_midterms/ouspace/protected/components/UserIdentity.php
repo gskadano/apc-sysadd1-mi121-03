931f8d6a6fdc3876de4df7cb06e2526e4d7d2748
Gene Anthony Kadano <gskadano@gmail.com>
2014-07-21 17:17:53 -0700

    generate model and crud for user table and edited UserIdentity.php and models/User.php for user login

--- trunk/midterm_test/Kadano_GeneAnthony_midterms/ouspace/protected/components/UserIdentity.php
+++ trunk/midterm_test/Kadano_GeneAnthony_midterms/ouspace/protected/components/UserIdentity.php
@@ -15,8 +15,10 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
+	private $_id;
 	public function authenticate()
 	{
+		/*
 		$users=array(
 			// username => password
 			'demo'=>'demo',
@@ -29,5 +31,24 @@ class UserIdentity extends CUserIdentity
 		else
 			$this->errorCode=self::ERROR_NONE;
 		return !$this->errorCode;
+		*/
+		
+		$username=strtolower($this->username);
+		$user=User::model()->find('LOWER(username)=?',array($username));
+		if($user===null){
+			$this->errorCode=self::ERROR_USERNAME_INVALID;
+		}else if(!$user->validatePassword($this->password)){
+			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		}else{
+			$this->_id=$user->id;
+			$this->username=$user->username;
+			$this->errorCode=self::ERROR_NONE;
+		}
+		return $this->errorCode==self::ERROR_NONE;
+		
+	}
+	
+	public function getId(){
+		return $this->_id;
 	}
 }
