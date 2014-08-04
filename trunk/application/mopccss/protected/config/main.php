85bb2ca60c9c3fce15cd661332195c9cc2e76d6a
Gene Anthony Kadano <gskadano@gmail.com>
2014-08-04 15:45:06 -0700

    updated database connection of mopccss/protected/config/main.php

--- trunk/application/mopccss/protected/config/main.php
+++ trunk/application/mopccss/protected/config/main.php
@@ -55,7 +55,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=erd_mop',
+			'connectionString' => 'mysql:host=localhost;dbname=mopccss',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
