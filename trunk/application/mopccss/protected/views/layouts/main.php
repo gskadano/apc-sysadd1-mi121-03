3efbde4c91e1a527ca8b42dac3092bef9fcf4a9e
John Emmanuel Tero <jejtero@gmail.com>
2014-08-07 16:53:50 +0800

    updated menus from mopccss page

--- trunk/application/mopccss/protected/views/layouts/main.php
+++ trunk/application/mopccss/protected/views/layouts/main.php
@@ -35,9 +35,9 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Baptismal', 'url'=>array('/baptismal/create')),
-				array('label'=>'Confirmation', 'url'=>array('/confirmation/create')),
-				array('label'=>'Marriage', 'url'=>array('/marriage/create')),
+				array('label'=>'Baptismal', 'url'=>array('/baptismal/create'),'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Confirmation', 'url'=>array('/confirmation/create'),'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Marriage', 'url'=>array('/marriage/create'),'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
