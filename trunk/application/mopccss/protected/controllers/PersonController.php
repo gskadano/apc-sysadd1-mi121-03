b07f5884a17c6c9437f495f166ee569fbc054f2e
Mark Anthony S.Andes <andesonline20@gmail.com>
2014-10-29 17:04:30 +0800

    Brought back the permissions of Person and Position to manage employee

--- trunk/application/mopccss/protected/controllers/PersonController.php
+++ trunk/application/mopccss/protected/controllers/PersonController.php
@@ -31,19 +31,14 @@ class PersonController extends Controller
 				'actions'=>array('index','view'),
 				'users'=>array('*'),
 			),
-			/*
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
 				'users'=>array('@'),
 			),
-			
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('create','update','admin'),
-				/*'user'=>array('admin'),
-				'users'=>array('@'),
-				'expression'=>'isset(Yii::app()->user->type) && 
-					((Yii::app()->user->type==="Regular"))'		//------------------------------------
-			),*/
+				'actions'=>array('admin','delete'),
+				'users'=>array('admin'),
+			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
 			),
--- trunk/application/mopccss/protected/controllers/PositionController.php
+++ trunk/application/mopccss/protected/controllers/PositionController.php
@@ -31,12 +31,12 @@ class PositionController extends Controller
 				'actions'=>array('index','view'),
 				'users'=>array('*'),
 			),
-			/*
+			
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
 				'users'=>array('@'),
 			),
-			
+			/*
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('create','update','admin'),
 				/*'user'=>array('admin'),
