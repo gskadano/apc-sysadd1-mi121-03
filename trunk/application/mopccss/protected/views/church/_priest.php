0a3d949526d6c34d2d3d3a07f6fde28846810f09
Gene Anthony Kadano <gskadano@gmail.com>
2014-10-30 23:34:21 -0700

    swap places of fields from baptismal/_form.php and baptismal/_form1.php and added update and delete to the priest records in church view.php

--- trunk/application/mopccss/protected/views/church/_priest.php
+++ trunk/application/mopccss/protected/views/church/_priest.php
@@ -3,6 +3,11 @@
 <br>
 <h1>Priest's Information</h1>
 <?php foreach ($conf as $row) { ?>
+<?php echo CHtml::link('<img src="' . Yii::app()->request->baseUrl . '/images/update.png" align="right"/>', 
+array('employee/update', 'id'=>$row->id)); ?>
+
+<?php echo CHtml::link('<img src="' . Yii::app()->request->baseUrl . '/images/delete.png" align="right"/>',
+	'#',array('submit'=>array('employee/delete','id'=>$row->id),'confirm' => 'Are you sure you want to delete?')); ?>  
 <?php $this->widget('zii.widgets.CDetailView', array(
                 'data'=>$row,
                 'attributes'=>array(   
--- trunk/application/mopccss/protected/views/church/view.php
+++ trunk/application/mopccss/protected/views/church/view.php
@@ -27,6 +27,21 @@ $this->menu=array(
 		'ch_address',
 	),
 )); ?>
+<?php
+  /*$this->widget('CMultiFileUpload', array(
+     'model'=>$model,
+     'attribute'=>'files',
+     'accept'=>'jpg|gif',
+     'options'=>array(
+        'onFileSelect'=>'function(e, v, m){ alert("onFileSelect - "+v) }',
+        'afterFileSelect'=>'function(e, v, m){ alert("afterFileSelect - "+v) }',
+        'onFileAppend'=>'function(e, v, m){ alert("onFileAppend - "+v) }',
+        'afterFileAppend'=>'function(e, v, m){ alert("afterFileAppend - "+v) }',
+        'onFileRemove'=>'function(e, v, m){ alert("onFileRemove - "+v) }',
+        'afterFileRemove'=>'function(e, v, m){ alert("afterFileRemove - "+v) }',
+     ),
+  ));*/
+?>
 <!------------------------------------------------------------------------------------------------------------------------------------>
 <br/><h2>Other Information</h2>
 <?php
