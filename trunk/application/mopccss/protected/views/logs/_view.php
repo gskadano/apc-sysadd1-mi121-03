fd3c4627078be718498c6265a163c2d6e451dbb2
Mark Anthony S. Andes <andesonline20@gmail.com>
2014-11-12 11:52:02 +0800

    Edit the logs/_view.php remove the link.

--- trunk/application/mopccss/protected/views/logs/_view.php
+++ trunk/application/mopccss/protected/views/logs/_view.php
@@ -5,8 +5,9 @@
 
 <div class="view">
 
+
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<?php echo CHtml::encode($data->id); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('employee_id')); ?>:</b>
