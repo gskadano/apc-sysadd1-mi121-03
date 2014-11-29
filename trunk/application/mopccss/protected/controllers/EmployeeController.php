73aa3944b0ce2721bbc9e326dd522efad48e87c2
Gene Anthony Kadano <gskadano@gmail.com>
2014-11-28 20:00:06 -0800

    no changes

--- trunk/application/mopccss/protected/controllers/EmployeeController.php
+++ trunk/application/mopccss/protected/controllers/EmployeeController.php
@@ -2,202 +2,205 @@
 
 class EmployeeController extends Controller
 {
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+        /**
+         * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+         * using two-column layout. See 'protected/views/layouts/column2.php'.
+         */
+        public $layout='//layouts/column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+        /**
+         * @return array action filters
+         */
+        public function filters()
+        {
+                return array(
+                        'accessControl', // perform access control for CRUD operations
+                        'postOnly + delete', // we only allow deletion via POST request
+                );
+        }
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','admin','delete'),
-				'users'=>array('@'),
-				'expression'=>'isset(Yii::app()->user->type) && 
-					((Yii::app()->user->type==="Admin"))'		//------------------------------------
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin'),
-				/*'user'=>array('admin'),*/
-				'users'=>array('@'),
-				'expression'=>'isset(Yii::app()->user->type) && 
-					((Yii::app()->user->type==="Regular"))'		//------------------------------------
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+        /**
+         * Specifies the access control rules.
+         * This method is used by the 'accessControl' filter.
+         * @return array access control rules
+         */
+        public function accessRules()
+        {
+                return array(
+                        array('allow',  // allow all users to perform 'index' and 'view' actions
+                                'actions'=>array('index','view'),
+                                'users'=>array('*'),
+                        ),
+                        array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                                'actions'=>array('create','update','admin','delete'),
+                                'users'=>array('@'),
+                                'expression'=>'isset(Yii::app()->user->type) &&
+                                        ((Yii::app()->user->type==="Admin"))'           //------------------------------------
+                        ),
+                        array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                                'actions'=>array('admin'),
+                                /*'user'=>array('admin'),*/
+                                'users'=>array('@'),
+                                'expression'=>'isset(Yii::app()->user->type) &&
+                                        ((Yii::app()->user->type==="Regular"))'         //------------------------------------
+                        ),
+                        array('deny',  // deny all users
+                                'users'=>array('*'),
+                        ),
+                );
+        }
 
 
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		if(Yii::app()->user->isGuest){ 
-			$this->redirect(array('/site/login'));
-		}else{
-			$this->render('view',array(
-				'model'=>$this->loadModel($id),
-			));
-		}
-	}
+        /**
+         * Displays a particular model.
+         * @param integer $id the ID of the model to be displayed
+         */
+        public function actionView($id)
+        {
+                if(Yii::app()->user->isGuest){
+                        $this->redirect(array('/site/login'));
+                }else{
+                        $this->render('view',array(
+                                'model'=>$this->loadModel($id),
+                        ));
+                }
+        }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new Employee;
+        /**
+         * Creates a new model.
+         * If creation is successful, the browser will be redirected to the 'view' page.
+         */
+        public function actionCreate()
+        {
+                $model=new Employee;
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-		
-		if(isset($_POST['Employee']))
-		{
-			//logs
-			$logC=new Logs;
-			$logC->employee_id= Yii::app()->user->id;
-			$logC->description= "Created an employee.";
-			$logC->dateTime= date('Y-m-d H:i:s');
-			
-			$model->attributes=$_POST['Employee'];
-			if($model->save() && $logC->save()){
-				$this->redirect(array('view','id'=>$model->id));
-			}
-		}
+                // Uncomment the following line if AJAX validation is needed
+                // $this->performAjaxValidation($model);
+               
+                if(isset($_POST['Employee']))
+                {
+                        $model->attributes=$_POST['Employee'];
+            $model->save();
+                       
+                //logs
+                        $logC=new Logs;
+                        $logC->employee_id= Yii::app()->user->id;
+                        $logC->description= "Employee <a href=/mopccss/index.php?r=employee/view&id=". $model->id . ">" . $model->FullName . "</a> has been created ";
+                        $logC->dateTime= date('Y-m-d H:i:s');
+                       
 
-		
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
-	
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+                        if($logC->save()){
+                                $this->redirect(array('view','id'=>$model->id));
+                        }
+                }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-		
-			//logs
-					$logU=new Logs;
-					$logU->employee_id= Yii::app()->user->id;
-					$logU->description= "Updated ". $model->FullName ." information.";
-					$logU->dateTime= date('Y-m-d H:i:s');
+               
+                $this->render('create',array(
+                        'model'=>$model,
+                ));
+        }
+       
+        /**
+         * Updates a particular model.
+         * If update is successful, the browser will be redirected to the 'view' page.
+         * @param integer $id the ID of the model to be updated
+         */
+        public function actionUpdate($id)
+        {
+                $model=$this->loadModel($id);
 
-		if(isset($_POST['Employee']))
-		{
-			$model->attributes=$_POST['Employee'];
-			if($model->save() && $logU->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+                // Uncomment the following line if AJAX validation is needed
+                // $this->performAjaxValidation($model);
+               
+                        //logs
+                                        $logU=new Logs;
+                                        $logU->employee_id= Yii::app()->user->id;
+                                        $logU->description= "Created an employee: <a href=/mopccss/index.php?r=employee/view&id=". $model->id . ">" . $model->id . "</a>". $model->FullName ." information.";
+                                        $logU->dateTime= date('Y-m-d H:i:s');
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+                if(isset($_POST['Employee']))
+                {
+                        $model->attributes=$_POST['Employee'];
+                        if($model->save() && $logU->save())
+                                $this->redirect(array('view','id'=>$model->id));
+                }
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		$this->loadModel($id)->delete();
+                $this->render('update',array(
+                        'model'=>$model,
+                ));
+        }
 
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			//$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-			$this->redirect(array('index'));
-	}
+        /**
+         * Deletes a particular model.
+         * If deletion is successful, the browser will be redirected to the 'admin' page.
+         * @param integer $id the ID of the model to be deleted
+         */
+        public function actionDelete($id)
+        {
+                $this->loadModel($id)->delete();
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		if(Yii::app()->user->isGuest){ 
-			$this->redirect(array('/site/login'));
-		}else{
-			$dataProvider=new CActiveDataProvider('Employee');
-			$this->render('index',array(
-				'dataProvider'=>$dataProvider,
-			));
-		}
-	}
+                // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+                if(!isset($_GET['ajax']))
+                        //$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+                        $this->redirect(array('index'));
+        }
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Employee('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Employee']))
-			$model->attributes=$_GET['Employee'];
+        /**
+         * Lists all models.
+         */
+        public function actionIndex()
+        {
+                if(Yii::app()->user->isGuest){
+                        $this->redirect(array('/site/login'));
+                }else{
+                        $dataProvider=new CActiveDataProvider('Employee');
+                        $this->render('index',array(
+                                'dataProvider'=>$dataProvider,
+                        ));
+                }
+        }
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        /**
+         * Manages all models.
+         */
+        public function actionAdmin()
+        {
+                $model=new Employee('search');
+                $model->unsetAttributes();  // clear any default values
+                if(isset($_GET['Employee']))
+                        $model->attributes=$_GET['Employee'];
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return Employee the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=Employee::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+                $this->render('admin',array(
+                        'model'=>$model,
+                ));
+        }
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param Employee $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='employee-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+        /**
+         * Returns the data model based on the primary key given in the GET variable.
+         * If the data model is not found, an HTTP exception will be raised.
+         * @param integer $id the ID of the model to be loaded
+         * @return Employee the loaded model
+         * @throws CHttpException
+         */
+        public function loadModel($id)
+        {
+                $model=Employee::model()->findByPk($id);
+                if($model===null)
+                        throw new CHttpException(404,'The requested page does not exist.');
+                return $model;
+        }
+
+        /**
+         * Performs the AJAX validation.
+         * @param Employee $model the model to be validated
+         */
+        protected function performAjaxValidation($model)
+        {
+                if(isset($_POST['ajax']) && $_POST['ajax']==='employee-form')
+                {
+                        echo CActiveForm::validate($model);
+                        Yii::app()->end();
+                }
+        }
 }
